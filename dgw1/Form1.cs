5c3e8cc26f5b06f8f230b689cd44f144a5421ee5
ali <asomson@ua.fm>
2016-05-22 09:47:00 +0300

    v1

---
+++ dgw1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dgw1
+{
+    public partial class Form1 : Form
+    {
+        class dg
+        {
+            public DataGridView name;
+            public enum values { integer, floating };
+            public void init(int max_column, int max_row, int begin_num = 0, int off_auto_zagolovok = 0)
+            {
+                name.Rows.Clear();
+                name.Columns.Clear();
+                int stro = 0;
+                if (off_auto_zagolovok == 0) for (int i = begin_num; i <= max_column; i++) name.Columns.Add("Column", i.ToString());
+                for (int i = begin_num; i < max_row; i++) name.Rows.Add();
+                for (int i = begin_num; i <= max_row; i++) name.Rows[stro++].HeaderCell.Value = i.ToString();
+                name.AutoResizeColumns();
+            }
+            public void rando(values valu = values.integer, int min = 0, int max = 65535)
+            {
+                Random a = new Random();
+                for (int i = 0; i < name.ColumnCount; i++)
+                    for (int j = 0; j < name.RowCount; j++)
+                        if (valu == values.integer) name.Rows[j].Cells[i].Value = a.Next(min, max);
+                        else name.Rows[j].Cells[i].Value = a.NextDouble() * (max - min) + min;
+                name.AutoResizeColumns();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
